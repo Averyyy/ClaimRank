7c990918d16c90863a7f1ca7b0fcbb9aaec82964
Li-Bo-github <1263899329@qq.com>
2024-11-06 10:31:32 -0600

    fix duplicate similarity

--- embedding/similarity.py
+++ embedding/similarity.py
@@ -7,7 +7,7 @@ index = faiss.read_index("./dataset/vector.index")
 total_documents = index.ntotal
 k = total_documents
 
-similarity_results = []
+similarity_results = set()
 
 for idx in range(total_documents):
     embedding = index.reconstruct(idx)
@@ -15,15 +15,17 @@ for idx in range(total_documents):
 
     distances, indices = index.search(embedding, k)
 
-    for i in range(1, k):
-        id1 = f"{idx:04}"
-        id2 = f"{indices[0][i]:04}"
+    for i in range(idx+1, k):
+        id1 = idx
+        id2 = indices[0][i]
+        pair = tuple(sorted([id1, id2]))
+
         similarity = 1 / (1 + distances[0][i])
+        # similarity = round(similarity, 4)
+        similarity_results.add((pair, similarity))
 
-        similarity_results.append((id1, id2, similarity))
-
+similarity_results = [(f"{pair[0]:04}", f"{pair[1]:04}", similarity) for pair, similarity in similarity_results]
 similarity_df = pd.DataFrame(similarity_results, columns=['id1', 'id2', 'similarity'])
-
 similarity_df.to_csv('./dataset/similarity_results.csv', index=False)
 
 print(similarity_df.head())
